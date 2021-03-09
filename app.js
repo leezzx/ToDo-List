660bf37a6b06fd9232bfabe6bc8a69d4bad3a53d
leezzx <79442805+leezzx@users.noreply.github.com>
2021-03-09 22:23:52 +0900

    Add file

---
+++ app.js
@@ -0,0 +1,37 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+//Functions
+
+function addTodo(event) {
+    event.preventDefault(); // prevent form from submitting
+    //todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Check Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append To List
+    todoList.appendChild(todoDiv);
+    //Clear Todo Input Value
+    todoInput.value = "";
+}
+
